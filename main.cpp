8816a76e6d8fac859f49eea692256eec36d5321d
Arjan <arjanjhamesm@gmail.com>
2025-10-23 21:37:33 +0800

    Complete training and results phase

--- main.cpp
+++ main.cpp
@@ -2,91 +2,145 @@
 #include <string>
 #include <vector> 
 #include <cmath>
+#include <conio.h>
 
 using std::cin; // namespace aliases
 using std::cout;
 using std::endl;
-using std::negate;
 using std::pow;
 using std::string;
 using std::vector;
 
-vector<double> training_temperature = {4.0, 5.0, 6.0, 7.0};
-vector<int> training_time = {5, 6, 7, 8};
+vector<double> training_temperature = {36.5, 36.8, 36.7, 37.0, 37.2, 37.1, 37.4, 37.6, 37.5, 37.8}; // dependent variable (y)
+vector<double> training_time = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // independent variable (x)
 
-double weight = 2.5;
-double bias = 1.2;
-double learning_rate = 0.001;
+double weight = 0.0;
+double bias = 0.0;
+double new_weight_gradient = 0.0;
+double new_bias_gradient = 0.0;
+double learning_rate = 0.0;
+int epochs = 0;
 
-double predict_output(const double &weight, const double &input, const double &bias) {
-    return (weight * input) + bias;
+int input_time = 0;
+
+bool is_on_training_phase = true;
+
+double predict_output(const double &weight, const double &input, const double &bias) { // of dependent variable at current data point, for computing error
+    return (weight * input) + bias; // y = w * x + b
 }
 
-double calculate_error(const double &actual, const double &predicted)
-{
-    return actual - predicted;
+double calculate_error(const double &actual, const double &predicted) { // for mean squared error and gradients
+    return actual - predicted; // y - y-hat i
 }
 
-double calculate_squared_error(const double &actual, const double &predicted) {
-    return pow((actual - predicted), 2.0);
-}
+double calculate_mean_squared_error(const double &weight, const double &bias, const vector<double> &dependent_variable_data, const vector<double> &independent_variable_data) { // loss (l), indicates how wrong predicted output (line) is
+    double sum_of_squared_error = 0.0; // store result of all loop iterations
 
-double calculate_mean_squared_error(const double &weight, const double &bias) {
-    double sum_of_squared_error = 0.0;
-
-    for (int y = 0; y < training_temperature.size(); y++) {
-        double squared_error = calculate_squared_error(training_temperature[y], predict_output(weight, training_time[y], bias));
+    for (size_t y = 0; y < dependent_variable_data.size(); y++) { // add up computed error for all dependent and independent variable data points
+        double squared_error = pow(calculate_error(dependent_variable_data[y], predict_output(weight, independent_variable_data[y], bias)), 2.0); // (sum of y i - y-hat i)^2
         sum_of_squared_error += squared_error;
     }
 
-    return (1.0 / static_cast<double>(training_temperature.size())) * sum_of_squared_error;
+    return (1.0 / static_cast<double>(dependent_variable_data.size())) * sum_of_squared_error; // L =  1 / n * (sum of y i - y-hat i)^2
 }
 
-double calculate_weight_gradient(const double &weight, const double &bias) {
-    double sum_of_gradient = 0.0;
+double calculate_weight_gradient(const double &weight, const double &bias, const vector<double> &dependent_variable_data, const vector<double> &independent_variable_data) {
+    double sum_of_gradient = 0.0; // store result of all loop iterations
 
-    for (int n = 0; n < training_temperature.size(); n++) {
-        double gradient = training_time[n] * (calculate_error(training_temperature[n], predict_output(weight, training_time[n], bias)));
+    for (size_t n = 0; n < dependent_variable_data.size(); n++)
+    {                                                                                                                                                               // add up computed gradients for all independent and dependent variable data points
+        double gradient = independent_variable_data[n] * (calculate_error(dependent_variable_data[n], predict_output(weight, independent_variable_data[n], bias))); // x i * (y i - y-hat i)
         sum_of_gradient += gradient;
     }
 
-    return -((2.0 / static_cast<double>(training_temperature.size())) * sum_of_gradient);
+    return -((2.0 / static_cast<double>(dependent_variable_data.size())) * sum_of_gradient); // gradient = -(2 / n) * (sum of x i * (y i - y-hat i))
 }
 
-double calculate_bias_gradient(const double &weight, const double &bias) {
-    double sum_of_error = 0.0;
+double calculate_bias_gradient(const double &weight, const double &bias, const vector<double> &dependent_variable_data, const vector<double> &independent_variable_data) {
+    double sum_of_error = 0.0; // store result of all loop iterations
 
-    for (int n = 0; n < training_temperature.size(); n++) {
-        double error = calculate_error(training_temperature[n], predict_output(weight, training_time[n], bias));
+    for (size_t n = 0; n < dependent_variable_data.size(); n++)
+    {                                                                                                                           // add up computed error for all dependent and independent variable data points
+        double error = calculate_error(dependent_variable_data[n], predict_output(weight, independent_variable_data[n], bias)); // y i - y-hat i
         sum_of_error += error;
     }
 
-    return -((2.0 / static_cast<double>(training_temperature.size())) * sum_of_error);
+    return -((2.0 / static_cast<double>(dependent_variable_data.size())) * sum_of_error); // gradient = -(2 / n) * (y i - y-hat i)
 }
 
-double update_using_new_gradient(double &parameter, const double &learning_rate, const double &gradient) {
-    parameter -= learning_rate * gradient;
+double update_using_new_gradient(double &parameter, const double &learning_rate, const double &gradient) { // compute new weight and bias
+    parameter = parameter - (learning_rate * gradient);
     return parameter;
 }
 
+bool check_if_on_training_phase() {
+    return is_on_training_phase;
+}
+
 int main() {
-    cout << "Weight: " << weight << endl;
-    cout << "Bias: " << bias << endl;
-    cout << "MSE: " << calculate_mean_squared_error(weight, bias) << endl;
+    // training phase
+    while (check_if_on_training_phase()) {
+        cout << "Enter initial weight (slope): " << endl;
+        cin >> weight;
 
-    double new_weight_gradient = calculate_weight_gradient(weight, bias);
-    cout << "New weight gradient: " << new_weight_gradient << endl;
+        cout << "Enter initial bias (y-intercept): " << endl;
+        cin >> bias;
 
-    double new_bias_gradient = calculate_bias_gradient(weight, bias);
-    cout << "New bias gradient: " << new_bias_gradient << endl;
+        cout << "Enter learning rate: " << endl;
+        cin >> learning_rate;
 
-    weight = update_using_new_gradient(weight, learning_rate, new_weight_gradient);
-    cout << "New weight: " << weight << endl;
+        cout << "Enter number of training iterations (epochs): " << endl;
+        cin >> epochs;
 
-    bias = update_using_new_gradient(bias, learning_rate, new_bias_gradient);
-    cout << "New bias: " << bias << endl;
+        for (int e = 0; e < epochs; e++)
+        {
+            cout << "Epoch " << e + 1 << ": " << endl;
+            cout << "Previous weight: " << weight << " | " << "Previous bias: " << bias << " | " << "Previous MSE: " << calculate_mean_squared_error(weight, bias, training_temperature, training_time) << endl;
 
-    cout << "MSE: " << calculate_mean_squared_error(weight, bias) << endl;
+            new_weight_gradient = calculate_weight_gradient(weight, bias, training_temperature, training_time);
+            new_bias_gradient = calculate_bias_gradient(weight, bias, training_temperature, training_time);
 
+            cout << "New weight gradient: " << new_weight_gradient << " | " << "New bias gradient: " << new_bias_gradient << endl;
+
+            weight = update_using_new_gradient(weight, learning_rate, new_weight_gradient);
+            bias = update_using_new_gradient(bias, learning_rate, new_bias_gradient);
+
+            cout << "New weight: " << weight << " | " << "New bias: " << bias << " | " << "New MSE: " << calculate_mean_squared_error(weight, bias, training_temperature, training_time) << endl;
+            cout << "============" << endl;
+        }
+
+        cout << endl;
+        cout << "Discard current parameters? (y/n)" << endl;
+
+        char input = ' ';
+
+        cin >> input;
+
+        switch (input)
+        {
+            case 'y':
+                weight = 0.0;
+                bias = 0.0;
+                learning_rate = 0.0;
+                epochs = 0;
+                break;
+            case 'n':
+                is_on_training_phase = false;
+                break;
+            default:
+                is_on_training_phase = false;
+                break;
+        }
+    }
+
+    // results phase
+    cout << endl;
+    cout << "Enter hour of the day to predict its temperature at (0-23): " << endl;
+    cin >> input_time;
+
+    cout << endl;
+    cout << "Predicted temperature (Celsius) at hour " << input_time << ": " << predict_output(weight, input_time, bias);
+
+    _getch();
     return 0;
 }
